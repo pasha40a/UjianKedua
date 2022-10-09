334e4767a91da93f2868b726c18525c49488df59
Addham Pasha Baihaqqi <pashabaihaqqi72@gmail.com>
2022-10-09 23:25:03 +0700

    First Test

---
+++ src/main/java/com/juaracoding/ujiandua/UjianSoalKesatu.java
@@ -0,0 +1,19 @@
+package com.juaracoding.ujiandua;
+
+public class UjianSoalKesatu {
+        public static void main(String[] args) {
+            for (int i = 9; i > 5; i--) {
+                for (int j = 1; j <= i; j++) {
+                    if (j% 2 ==1) {
+                        System.out.print(j);
+                    }else {
+                        System.out.print("_");
+                    }
+                }
+
+                System.out.println();
+            }
+
+        }
+    }
+
